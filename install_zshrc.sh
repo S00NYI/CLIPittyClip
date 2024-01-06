ca9f924deec56c7dbb282761a2563f85b92fa43d
SoonYi <55300935+S00NYI@users.noreply.github.com>
2024-01-06 02:44:53 -0500

    add new info for users, also correct export PATH, added permission for CLIPittyClip programs.

--- install_zshrc.sh
+++ install_zshrc.sh
@@ -1,18 +1,21 @@
 # Get the absolute path of the directory containing this script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(pwd)"
 
 # Check if the script directory is already in PATH
 if [[ ":$PATH:" == *":$SCRIPT_DIR:"* ]]; then
   echo "The directory is already in PATH. CLIPittyClip is probably already installed."
 else
-  echo "This installation script will add the current directory to your PATH and give execute permission so CLIPittyClip can be run natively as a command-line program."
+  echo "This installation script will add the current directory to your PATH and give execute permission so CLIPittyClip programs can be run natively as a command-line program."
+  echo "Directory to add: $SCRIPT_DIR"
+  echo "Make sure the directory above is the directory containing CLIPittyClip programs. If not, please traverse to the correct directory."
   read -n 1 -r -p "Do you want to proceed? (Press any key to continue or 'Ctrl+C' to cancel) "
   echo
 
   echo "Adding $SCRIPT_DIR to PATH..."
-  echo "PATH=$PATH:$SCRIPT_DIR" >> ~/.zshrc
+  echo 'export PATH="$PATH:'$(pwd)'"' >> ~/.zshrc
   chmod +x "$SCRIPT_DIR/CLIPittyClip.sh"
-  chmod +x "$SCRIPT_DIR/CombinedPeakCalling.sh"
-  echo "Done! You can now run CLIPittyClip.sh in directory containing your fastq.gz file."
+  chmod +x "$SCRIPT_DIR/MAPittyMap.sh"
+  chmod +x "$SCRIPT_DIR/PEAKittyPeak.sh"
+  echo "Done!"
   echo "Make sure to restart your terminal or run 'source ~/.zshrc'."
 fi
